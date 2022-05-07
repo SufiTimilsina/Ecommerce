87267d1b192496a1795026c707aee47e485fce23
SufiTimilsina <105101057+SufiTimilsina@users.noreply.github.com>
2022-05-07 15:53:20 +0545

    Update settings.py

--- Eshop/Eshop/settings.py
+++ Eshop/Eshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+3=y29o9@smavi-kl3)9=$752nrvp8xz%0yh2e7#zs*x9iibtl'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
